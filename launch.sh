0c07632a6aa4fe75b5093382c15a5023014b148e
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-02-22 11:46:00 +0530

    update

--- launch.sh
+++ launch.sh
@@ -1,29 +1,16 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
-CFG="$HOME/.config/eww"
-EWW=`which eww`
+CFG="/data/data/com.termux/files/home/.config/eww"
+EWW=$(command -v eww)
 
 ## run eww daemon if not running already
-if [[ ! `pidof eww` ]]; then
-	${EWW} daemon
-	sleep 1
+if [[ ! $(pidof eww) ]]; then
+    ${EWW} daemon
+    sleep 1
 fi
 
-## open widgets 
+## open widgets
 run_eww() {
-	${EWW} --config "$CFG" open-many \
-		   profile \
-		   clock \
-		   uptime
+    ${EWW} --config "$CFG" open-many profile clock uptime
 }
 run_eww
-
-## launch or close widgets accordingly
-# if [[ ! -f "$FILE" ]]; then
-# 	touch "$FILE"
-# 	run_eww
-# else
-# 	${EWW} --config "$CFG" close \
-# 					profile clock uptime
-# 	rm "$FILE"
-# fi
